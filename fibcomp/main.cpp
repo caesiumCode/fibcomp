a0861cc0c44e0c1aa2fa37695ae245e8bd8e0eb0
caesiumCode <caesiumcode@aol.com>
2024-07-20 16:24:53 +0200

    Initial Commit

---
+++ fibcomp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fibcomp
+//
+//  Created by Stephen Jaud on 20/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
