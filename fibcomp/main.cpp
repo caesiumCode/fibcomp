79bdd16a1d751d65582dfa7504a4cdfd4bab361e
caesiumCode <caesiumcode@aol.com>
2024-07-26 17:07:37 +0200

    factor main.cpp

--- fibcomp/main.cpp
+++ fibcomp/main.cpp
@@ -13,8 +13,6 @@
 #include "fibonacci_binet.hpp"
 #include "fibonacci_fmat_triangle.hpp"
 
-BinetCouple binet_power(uint64_t);
-
 double running_time(Fibonacci& algo, uint64_t n)
 {
     const auto start = std::chrono::steady_clock::now();
@@ -52,60 +50,80 @@ void disp_search(Fibonacci& algo, double time_limit)
     std::cout << std::setw(20) << algo.name << " |" << std::setw(10) << search_threshold(algo, time_limit, algo.slow) << std::endl;
 }
 
-int main(int argc, const char * argv[]) 
+void find_thresholds(double time_limit = 1)
 {
-    
-    double time_limit = 1;
-    
     std::vector<std::shared_ptr<Fibonacci>> algos;
     
     algos.push_back(std::make_shared<FibonacciRec>());
-    //algos.push_back(std::make_shared<FibonacciRecMem>());
     algos.push_back(std::make_shared<FibonacciLin>());
     algos.push_back(std::make_shared<FibonacciMatRec>());
     //algos.push_back(std::make_shared<FibonacciMatLoop>());
     //algos.push_back(std::make_shared<FibonacciFmat>());
-    //algos.push_back(std::make_shared<FibonacciFmatMem>());
+    algos.push_back(std::make_shared<FibonacciFmatMem>());
     algos.push_back(std::make_shared<FibonacciFmatTriangle>());
     algos.push_back(std::make_shared<FibonacciBinet>());
     
     std::cout << std::setw(20) << "ALGORITHME" << " |" << std::setw(10) << "MAX N" << std::endl;
     std::cout << std::string(32, '-') << std::endl;
     for (std::shared_ptr<Fibonacci> algo : algos) disp_search(*algo.get(), time_limit);
+}
+
+void    compare_to_ref();
+double  running_time_high_precision(Fibonacci&, uint64_t, uint64_t = 10);
+
+int main(int argc, const char * argv[]) 
+{
+    //find_thresholds();
     
-    /*
-    uint64_t n = 100000;
-    FibonacciLin algo;
-    uintinf_t ref = algo.run(n);
-    for (std::shared_ptr<Fibonacci> algo : algos)
-    {
-        bool good = algo->run(n).is_equal(ref);
-        std::cout << std::setw(20) << algo->name << std::setw(12) << (good ? "GOOD" : "BAD") << std::endl;
-    }
-     */
+    //compare_to_ref();
     
-    //std::cout << res_1.to_string() << std::endl;
-    //std::cout << res_2.to_string() << std::endl;
-    
-    /*
-    FibonacciFmatMem algo;
-    uint64_t n = 15000000;
-    double T = 0;
-    int N = 20;
-    for (int i = 0; i < N; i++)
-    {
-        double t = running_time(algo, n);
-        std::cout << t << std::endl;
-        T += t;
-    }
-    std::cout << "avg : " << T / double(N) << std::endl;
-    */
-    
-    /*
-    FibonacciFmatMem algo;
-    std::cout << running_time(algo, 13000000) << std::endl;
-     */
+    FibonacciFmatTriangle algo;
+    std::cout << running_time_high_precision(algo, 15000000, 100) << "s" << std::endl;;
     
     
     return EXIT_SUCCESS;
+}
+
+void compare_to_ref()
+{
+    FibonacciLin algo_ref;
+    
+    std::vector<std::shared_ptr<Fibonacci>> algos;
+    algos.push_back(std::make_shared<FibonacciMatRec>());
+    algos.push_back(std::make_shared<FibonacciMatLoop>());
+    algos.push_back(std::make_shared<FibonacciFmat>());
+    algos.push_back(std::make_shared<FibonacciFmatMem>());
+    algos.push_back(std::make_shared<FibonacciFmatTriangle>());
+    algos.push_back(std::make_shared<FibonacciBinet>());
+    
+    std::cout << std::setw(10) << "n";
+    for (std::shared_ptr<Fibonacci> algo : algos)
+    {
+        std::cout << std::setw(21) << algo->name;
+    }
+    std::cout << std::endl;
+    
+    for (int i = 0; i < 100; i++)
+    {
+        uint64_t n = rand() % 300000;
+        
+        uintinf_t ref = algo_ref.run(n);
+        
+        std::cout << std::setw(10) << n;
+        for (std::shared_ptr<Fibonacci> algo : algos)
+        {
+            bool good = algo->run(n).is_equal(ref);
+            std::cout << std::setw(21) << (good ? "GOOD" : "X");
+        }
+        
+        std::cout << std::endl;
+    }
+}
+
+double running_time_high_precision(Fibonacci& algo, uint64_t n, uint64_t N)
+{
+    double T = 0;
+    for (std::size_t i = 0; i < N; i++) T += running_time(algo, n);
+    
+    return T / double(N);
 }
